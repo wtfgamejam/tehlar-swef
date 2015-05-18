00da4284410a886151636704169c70fb84d5f397
Tim Bellefleur <nomoon@phoebus.ca>
2015-05-18 02:12:23 -0700

    Utility Methods

---
+++ Assets/Scripts/Tim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Tim
+{
+    public static class UtilityMethods
+    {
+        public static float RoundTo(float f, int precision)
+        {   
+            return Mathf.Round(f * 10 * precision) / (10 * precision); 
+        }
+        
+        public static Vector3 V2toV3(Vector2 f)
+        {
+            return new Vector3(f.x, f.y);
+        }
+    }
+}
